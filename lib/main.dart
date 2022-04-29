62f744841f9da8baa999bb3745fd33d7cd072669
Takashi Fukuyama <osakalivetheater@gmail.com>
2022-04-29 11:45:03 +0900

    1回目

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:osakalivetheater2/book_list/book_list_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
--- lib/register/register_model.dart
+++ lib/register/register_model.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+
 class RegisterModel extends ChangeNotifier {
   final titleController = TextEditingController();
   final authorController = TextEditingController();
